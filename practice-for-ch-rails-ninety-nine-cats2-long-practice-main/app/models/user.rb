bd716fc20d0d6af5ae950efd2fc403dc8c1dfc8f
Caroline Zhang <cczhang1@yahoo.com>
2024-02-19 13:36:26 -0800

    Finish User model

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    attr_reader :password
+
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :session_token, uniqueness: true
+
+    before_validation :ensure_session_token
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(password)
+
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user 
+        else
+            return nil 
+        end
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+end
